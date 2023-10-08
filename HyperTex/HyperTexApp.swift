7d98c19ce6ad239026988a4c3ab71342086191fd
Dom Panaia <dompanaia@gmail.com>
2023-10-07 23:41:30 -0400

    New Project
    Html viewer

---
+++ HyperTex/HyperTexApp.swift
@@ -0,0 +1,18 @@
+//
+//  HyperTexApp.swift
+//  HyperTex
+//
+//  Created by Dom Panaia on 2023-10-02.
+//
+
+import SwiftUI
+import WebKit
+
+@main
+struct HyperTexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
