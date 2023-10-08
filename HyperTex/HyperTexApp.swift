82033f1f6949404820e208382d3715676642ca85
Dom Panaia <dompanaia@gmail.com>
2023-10-08 02:31:46 -0400

    It works!

--- HyperTex/HyperTexApp.swift
+++ HyperTex/HyperTexApp.swift
@@ -1,10 +1,3 @@
-//
-//  HyperTexApp.swift
-//  HyperTex
-//
-//  Created by Sythatic on 2023-10-02.
-//
-
 import SwiftUI
 import WebKit
 
@@ -14,5 +7,53 @@ struct HyperTexApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+struct ContentView: View {
+    @State private var fileURL: URL?
+    
+    var body: some View {
+        VStack {
+            WebView(fileURL: $fileURL)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .toolbar {
+            Button("Open File") {
+                openFile()
+            }
+        }
+    }
+    
+    private func openFile() {
+        let openPanel = NSOpenPanel()
+        openPanel.allowedContentTypes = [.html]
+        
+        openPanel.begin { result in
+            if result == .OK, let selectedFileURL = openPanel.url {
+                self.fileURL = selectedFileURL
+            }
+        }
+    }
+}
+
+struct WebView: NSViewRepresentable {
+    @Binding var fileURL: URL?
+    
+    func makeNSView(context: Context) -> WKWebView  {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        if let fileURL = fileURL {
+            nsView.loadFileURL(fileURL, allowingReadAccessTo: fileURL)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
