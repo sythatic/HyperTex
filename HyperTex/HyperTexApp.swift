388fc2746851392d21a7ae357cd2c17b22196f15
Dom Panaia <dompanaia@gmail.com>
2023-10-08 15:12:47 -0400

    Update to beta 2

--- HyperTex/HyperTexApp.swift
+++ HyperTex/HyperTexApp.swift
@@ -3,32 +3,29 @@ import WebKit
 
 @main
 struct HyperTexApp: App {
+    @State private var fileURL: URL?
+    @State private var urlString: String = ""
+    @State private var reloadFlag = true
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(fileURL: $fileURL, urlString: $urlString, reloadFlag: $reloadFlag)
         }
-    }
-}
-
-struct ContentView: View {
-    @State private var fileURL: URL?
-    
-    var body: some View {
-        VStack {
-            WebView(fileURL: $fileURL)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .toolbar {
-            Button("Open File") {
-                openFile()
+        
+        .commands {
+            /* CommandMenu("DEBUG") {
+                Button("MORE DEBUG") { print("triggered debug") }
+            } */
+            CommandGroup(replacing: .newItem) {
+                Button("Open") { openFile() }
             }
         }
     }
-    
+
     private func openFile() {
         let openPanel = NSOpenPanel()
         openPanel.allowedContentTypes = [.html]
-        
+
         openPanel.begin { result in
             if result == .OK, let selectedFileURL = openPanel.url {
                 self.fileURL = selectedFileURL
@@ -37,23 +34,79 @@ struct ContentView: View {
     }
 }
 
-struct WebView: NSViewRepresentable {
+struct ContentView: View {
     @Binding var fileURL: URL?
-    
-    func makeNSView(context: Context) -> WKWebView  {
-        let webView = WKWebView()
-        return webView
+    @Binding var urlString: String
+    @Binding var reloadFlag: Bool
+
+    var body: some View {
+        VStack {
+            WebView(fileURL: $fileURL, reloadFlag: $reloadFlag)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .toolbar {
+            /* ToolbarItem(placement: .primaryAction) {
+                TextField("Enter a file path", text: $urlString, onCommit: {
+                    if let url = URL(string: urlString) {
+                        self.fileURL = url
+                    }
+                })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 256)
+            } */
+            ToolbarItem(placement: .primaryAction) {
+                Button("Open") { openFile() }
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button("Refresh") { reloadFlag.toggle() }
+                .disabled(fileURL == nil)
+            }
+            /* ToolbarItem(placement: .primaryAction) {
+                Button("Inspector") { openInTextEditor() }
+                .disabled(fileURL == nil)
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button("DEBUG") {print("triggered debug")}
+            } */
+        }
     }
-    
-    func updateNSView(_ nsView: WKWebView, context: Context) {
+
+    private func openFile() {
+        let openPanel = NSOpenPanel()
+        openPanel.allowedContentTypes = [.html]
+
+        openPanel.begin { result in
+            if result == .OK, let selectedFileURL = openPanel.url {
+                self.fileURL = selectedFileURL
+            }
+        }
+    }
+
+    private func openInTextEditor() {
         if let fileURL = fileURL {
-            nsView.loadFileURL(fileURL, allowingReadAccessTo: fileURL)
+            NSWorkspace.shared.open(fileURL)
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct WebView: NSViewRepresentable {
+    @Binding var fileURL: URL?
+    @Binding var reloadFlag: Bool
+
+    func makeNSView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        if let fileURL = fileURL {
+            nsView.loadFileURL(fileURL, allowingReadAccessTo: fileURL)
+        }
+
+        if reloadFlag {
+            nsView.reload()
+            reloadFlag = false // Reset the reload flag
+        }
     }
 }
+
--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 
 # `HyperTex`
-![Static Badge](https://img.shields.io/badge/v1.0.0--b.1-BETA-orange)
+![Static Badge](https://img.shields.io/badge/v1.0.0--b.2-BETA-blue)
 #### A simple macOS app that allows users to view HTML files without the overhead of a full browser.
 </div>
 
-### Planned Features & Support
+### Platform Support
 
-- [x] macOS  `IN PROGRESS`
-- [ ] Windows  `NOT STARTED`
+![Static Badge](https://img.shields.io/badge/macOS-IN_BETA-blue)
+![Static Badge](https://img.shields.io/badge/Windows-NOT_STARTED-critical)
 
-### View and edit
-- [x] HTML & CSS Files
-- [ ] Markdown Files
-- [ ] Compiled HTML Help (.chm)
+### Features
+![Static Badge](https://img.shields.io/badge/HTML_&_CSS_Files-IN_BETA-blue)
+![Static Badge](https://img.shields.io/badge/Markdown_Files-IN_BETA-blue)
+![Static Badge](https://img.shields.io/badge/Compiled_HTML_Help_(.chm)-NOT_STARTED-critical)
+
+### Known Issues
+
+![Static Badge](https://img.shields.io/badge/New_files_don't_automatically_reload_when_opened_after_using_reload-MINOR-yellow)
+![Static Badge](https://img.shields.io/badge/Manual_path_entry_does_not_work-MINOR-yellow)
